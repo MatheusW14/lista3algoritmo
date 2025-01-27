a1fec6cd2384f1b4f79eca866d34f63dcbdaaa87
Matheus Costa Mendes <matheuscostamendes@hotmail.com>
2025-01-27 19:48:49 -0300

    add function to filter even numbers from user input

---
+++ ex2.py
@@ -0,0 +1,19 @@
+def numero_par_na_lista():
+    """
+    Solicita ao usuário uma sequência de números inteiros separados por espaço,
+    filtra os números pares e retorna uma lista contendo esses números pares.
+    Returns:
+        list: Uma lista de números inteiros pares.
+    """
+
+    entrada = input("Digite uma sequencia de numeros inteiros separados por espaço: ")
+
+    numeros_pares = list(
+        filter(lambda x: x % 2 == 0, map(int, entrada.split()))
+    )  # Converte os números da entrada (string) em inteiros. Filtra apenas os números pares.
+
+    return numeros_pares
+
+
+numeros_pares = numero_par_na_lista()
+print("Os números pares na lista são:", numeros_pares)
